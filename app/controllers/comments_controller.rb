d25a7ed7fd242190ca69290254808d8d2cd2d605
fujikyo-1214 <hatumike@icloud.com>
2021-03-01 15:40:02 +0900

    コントローラー修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
     if @comment.save
-      redirect_to prototype_path(@comment.prototype)
+      redirect_to eat_path(@comment.eat)
     else
       @eat = @comment.prototype
       @comments = @eat.comments
